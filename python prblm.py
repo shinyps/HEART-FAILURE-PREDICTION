613e8269ba1e67c2d20fe3ecf126c2f76230836d
shinyps <90965789+shinyps@users.noreply.github.com>
2021-09-24 20:14:57 +0530

    Add files via upload

---
+++ python prblm.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+## 1.num varv
+x=10
+y=20
+print(x)
+print(y)
+
+
+# In[3]:
+
+
+## 2.cal in python
+import math
+math.pow(2,4)
+
+
+# In[ ]:
+
+
+## 3.BODMAS 
+
+
+# In[9]:
+
+
+## 4. LIST
+L=['DAIRYMILK','KITKAT','MARS']
+print(L)
+
+
+# In[10]:
+
+
+print(len(L))
+
+
+# In[17]:
+
+
+## 5. change a value in list
+a=['dairymilk','kitkat','mars','milkybar','fivestar']
+a[1:3]=['watermelon','apple','orange','cherry']
+print(a)
+
+
+# In[12]:
+
+
+## 6.How do we extract values from list?
+a=['dairymilk','kitkat','milkybar','mars']
+
+
+# In[14]:
+
+
+print(a[0:2])
+
+
+# In[24]:
+
+
+## 7.How we create loops in python using list?
+l=['dairymilk','kitkat','mars']
+for i in l:
+    print(i)
+
+
+# In[19]:
+
+
+## 8. interchange 
+a=[1,2,3]
+b=[4,5,6]
+a,b=b,a
+
+
+# In[20]:
+
+
+print(a,b)
+
+
+# In[ ]:
+
+
+
+
